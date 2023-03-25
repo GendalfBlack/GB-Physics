82153eb8477f478c4dc1d57aedc276201fbabf13
GendalfBlack <garmash_a@itstep.academy>
2023-03-25 02:46:53 +0200

    InitialVector pointing in direction of starting velosity in respect to values of text boxes and changing dynamically on input

--- Projectile Motion Simulator/Projectile Motion Simulator/MainWindow.xaml.cs
+++ Projectile Motion Simulator/Projectile Motion Simulator/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -30,17 +31,20 @@ namespace Projectile_Motion_Simulator
         DispatcherTimer plotTimer = new DispatcherTimer();
         double t, x, y0, v0, theta, vx, vy, g, maxHeight, maxDistance;
         Polyline trajectoryPlot = new Polyline();
-        PointCollection points = new PointCollection();
+        Line initialVector = new Line();
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
             plotTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
             plotTimer.Tick += new EventHandler(plotTimer_Tick);
             timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
             timer.Tick += new EventHandler(timer_Tick);
-            trajectoryPlot.Stroke = System.Windows.Media.Brushes.Black;
+            trajectoryPlot.Stroke = Brushes.Black;
             trajectoryPlot.StrokeThickness = 2;
             trajectoryPlot.StrokeDashArray = new DoubleCollection() { 4, 4 };
+            initialVector.Stroke = Brushes.Red;
+            initialVector.StrokeThickness = 3;
             mainCanvas.Children.Add(trajectoryPlot);
+            mainCanvas.Children.Add(initialVector);
             Setup();
         }
         private void Pause_Click(object sender, RoutedEventArgs e)
@@ -50,24 +54,33 @@ namespace Projectile_Motion_Simulator
             if (plotTimer.IsEnabled) { plotTimer.Stop(); }
             else { plotTimer.Start(); }
         }
-        private void Reset_Click(object sender, RoutedEventArgs e)
+        private void angleTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Setup();
+        }
+        private void v0TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             Setup();
         }
         private void y0TextBox_TextInput(object sender, TextChangedEventArgs e)
         {
-            try { ResetProjectile(Convert.ToDouble(y0TextBox.Text)); } catch (Exception) { }
+            Setup();
+        }
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            Setup();
         }
         private void ResetProjectile(double dy)
         {
+            var p = ProjectileEllipse;
             if (mainCanvas != null)
             {
                 double h = mainCanvas.ActualHeight;
-                double ph = ProjectileEllipse.ActualHeight;
+                double ph = p.ActualHeight;
                 y0 = h - ph - dy;
-                if (y0 >= h - ph){ Canvas.SetTop(ProjectileEllipse, h - ph); }
-                else { Canvas.SetTop(ProjectileEllipse, y0); }
-                Canvas.SetLeft(ProjectileEllipse, 0);
+                if (y0 >= h - ph){ Canvas.SetTop(p, h - ph); }
+                else { Canvas.SetTop(p, y0); }
+                Canvas.SetLeft(p, 0);
             }
             trajectoryPlot.Points.Clear();
         }
@@ -77,13 +90,35 @@ namespace Projectile_Motion_Simulator
             maxDistance = 0;
             t = 0;
             x = 0;
-            try{ ResetProjectile(Convert.ToDouble(y0TextBox.Text)); } catch (Exception) { }
+            if (y0TextBox == null ){ return; }
+            if (angleTextBox == null){ return; }
+            if (v0TextBox == null ){ return; }
+            if (gravityTextBox == null ){ return; }
+            if (!Regex.IsMatch(y0TextBox.Text, @"^-?\d*,?\d+$")){ return; }
+            if (!Regex.IsMatch(v0TextBox.Text, @"^-?\d*,?\d+$")){ return; }
+            if (!Regex.IsMatch(angleTextBox.Text, @"^-?\d*,?\d+$")){ return; }
+            if (!Regex.IsMatch(gravityTextBox.Text, @"^-?\d*,?\d+$")){ return; }
+            ResetProjectile(Convert.ToDouble(y0TextBox.Text));
             v0 = Convert.ToDouble(v0TextBox.Text);
             theta = -Convert.ToDouble(angleTextBox.Text) * Math.PI / 180;
             vx = v0 * Math.Cos(theta);
             vy = v0 * Math.Sin(theta);
             g = -Convert.ToDouble(gravityTextBox.Text);
             UpdateStatusBar();
+            SetupInitialVector();
+        }
+        private void SetupInitialVector()
+        {
+            var p = ProjectileEllipse;
+            if (p != null)
+            {
+                double x = (double)p.GetValue(Canvas.LeftProperty);
+                double y = (double)p.GetValue(Canvas.TopProperty);
+                initialVector.X1 = x + p.Width / 2;
+                initialVector.Y1 = y + p.Height / 2;
+                initialVector.X2 = x + v0 * Math.Cos(theta) + p.Width / 2;
+                initialVector.Y2 = y + v0 * Math.Sin(theta) + p.Height / 2;
+            }           
         }
         private void Start_Click(object sender, RoutedEventArgs e)
         {
@@ -94,8 +129,8 @@ namespace Projectile_Motion_Simulator
         private void plotTimer_Tick(object sender, EventArgs e)
         {
             var p = ProjectileEllipse;
-            double x = (double)p.GetValue(Canvas.LeftProperty);
-            double y = (double)p.GetValue(Canvas.TopProperty);
+            double x = (double)p.GetValue(Canvas.LeftProperty) + p.Width / 2;
+            double y = (double)p.GetValue(Canvas.TopProperty) + p.Width / 2;
             trajectoryPlot.Points.Add(new Point(x, y));
         }
         private void timer_Tick(object sender, EventArgs e)
@@ -116,7 +151,7 @@ namespace Projectile_Motion_Simulator
             var p = ProjectileEllipse;
             double x = (double)p.GetValue(Canvas.LeftProperty);
             double h = mainCanvas.ActualHeight;
-            double ph = ProjectileEllipse.ActualHeight;
+            double ph = p.ActualHeight;
             double y = h - ph - (double)p.GetValue(Canvas.TopProperty);
             if (y < 0) { y = 0; }
             maxDistance = x > maxDistance ? x : maxDistance;
